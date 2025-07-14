4fdf7722c54c218115a8135e90e1748e5bdaa9d8
Bekzo <forgithub243@gmail.com.com>
2025-07-15 00:11:54 +0500

    fix: add WhiteNoiseMiddleware for static

--- itcaser/settings.py
+++ itcaser/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +125,8 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # (если нужно)
 STATICFILES_DIRS = [BASE_DIR / 'main/static']
