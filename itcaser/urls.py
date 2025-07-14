c669792f7bbb005b0a40ed9d97c2b589358d8615
Bekzo <forgithub243@gmail.com.com>
2025-07-14 23:23:10 +0500

    fix: remove unused click.urls import

--- itcaser/urls.py
+++ itcaser/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('payments/', include('payments.urls')),
-    path('payments/', include('click.urls')),
+    # path('payments/', include('click.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
